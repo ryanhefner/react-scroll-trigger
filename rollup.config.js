620912e04b57c88c51069f8124966b41f3e7a227
Ryan Hefner <hi@ryanhefner.com>
2022-01-28 22:01:00 -0500

    Update Rollup to use terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
-import { uglify } from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const config = {
@@ -36,7 +36,7 @@ const config = {
 };
 
 if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(uglify());
+  config.plugins.push(terser());
 }
 
 export default config;
